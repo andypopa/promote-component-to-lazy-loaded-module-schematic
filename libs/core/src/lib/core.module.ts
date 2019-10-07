8ec5b1c9151c401fbfbe4b935e237e65711e746d
Andrei Popa <andypopa@gmail.com>
2019-10-07 18:33:10 +0300

    fix: core module decls

--- libs/core/src/lib/core.module.ts
+++ libs/core/src/lib/core.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MineComponent } from './mine/mine.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [MineComponent]
+  imports: [CommonModule]
 })
 export class CoreModule {}
