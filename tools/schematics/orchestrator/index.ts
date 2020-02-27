2cb864f7adf35f163eedc489a9c07520ccb400ac
Andrei Popa <andypopa@gmail.com>
2020-02-28 01:17:35 +0200

    fix: uncommented

--- tools/schematics/orchestrator/index.ts
+++ tools/schematics/orchestrator/index.ts
@@ -1,11 +1,11 @@
-import { chain, externalSchematic, Rule, noop } from '@angular-devkit/schematics';
+import { chain, externalSchematic, Rule, noop, branchAndMerge } from '@angular-devkit/schematics';
 
 export default function(schema: any): Rule {
   return chain([
-    // externalSchematic('.', 'create-new-dir', {
-    //   project: 'qwerty',
-    //   componentClassName: 'MapComponent'
-    // }),
+    externalSchematic('.', 'create-new-dir', {
+      project: 'qwerty',
+      componentClassName: 'MapComponent'
+    }),
     externalSchematic('.', 'promote-component-to-lazy-loaded-module', {
       project: 'qwerty',
       componentClassName: 'MapComponent'
